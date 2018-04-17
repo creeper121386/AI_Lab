7eac03f96f437631c452a5d02b68786498159d65
creeper121386 <694029828@qq.com>
2018-04-17 12:18:00 +0800

    快写完啦!

--- Task_week2/Laplace-bayes.py
+++ Task_week2/Laplace-bayes.py
@@ -1,48 +1,64 @@
 import numpy as np
 import csv
+test_num = 1
 
 
 def load():
     with open("/media/why/DATA/why的程序测试/AI_Lab/Task/Task_week2/melon.csv", "r") as f:
         reader = csv.reader(f)
         data = [x[1:] for x in reader]
-
-
-
-
-    for i in range(len(data[0])):
+    data = data[1:]
+    for i in range(len(data[0])-3):
+        feature = []
+        value = []
+        num = 0
         for j in range(len(data)):
-            
-
-
-
-
+            tmp = data[j][i]
+            if tmp not in feature:
+                feature.append(tmp)
+                value.append(num)
+                data[j][i] = num
+                num += 1
+            else:
+                data[j][i] = value[feature.index(tmp)]
 
     data = np.array(data[1:])
     label = [(1 if x == '是' else 0) for x in data[:, -1:]]
-    tmp = np.array(data[:, -3:-1], 'float32')
-    chinese=data[:,:-3]
-    print(chinese[0][0])
-    
-    for i in range(len(chinese[0])):
-        feature={}
-        j=0
-        for x in chinese[:,i:i+1]:
-            if x not in feature:
-                feature[x]=j
-                j+=1
-        for j in range(len(chinese)):
-            chinese[j][i]=feature[chinese[j][i]]
+    data = np.array(data[:, :-1], 'float32')
 
-
-    train_data = np.column_stack((data[:, 0:-3], tmp))
-    print(type(train_data[0][-1]))
-    return train_data, train_label
+    train_data = data[:-test_num, :]
+    train_label = label[:-test_num]
+    test_data = data[-test_num:, :]
+    test_label = label[-test_num:]
+    return train_data, train_label, test_data, test_label
 
 
 def train(train_data, train_label):
     p_c1 = train_label.count(1)/len(train_label)
-    p_c2 = 1-p_c1
+    p_c0 = 1-p_c1
+    num = len(train_label)
+    length = len(train_data[0])
+    p_x1 = []
+    p_x0 = []
+    tmp=[[],[]]
+    for i in range(num):
+        if train_label[i] == 1:
+            tmp[1].append(train_data[i])
+        else :
+            tmp[0].append(train_data[i])
+
+    for y in tmp:
+        for j in range(length):
+            column = [x[0] for x in (y[:, j:j+1]).tolist()]
+            prob = {}
+            if j < 6:
+                feature = set(column)
+                for x in feature:
+                    prob[x] = column.count(x)/train_label.count(1)
+                p_x1.append(prob)
+            else:
+                pass
 
 
-train_data, train_label = load()
+train_data, train_label, test_data, test_label = load()
+train(train_data, train_label)
