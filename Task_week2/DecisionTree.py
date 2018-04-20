580044a91ae5e05929a2c653baf27d3439066085
creeper121386 <694029828@qq.com>
2018-04-20 21:10:39 +0800

    emmm没什么进展

--- Task_week2/DecisionTree.py
+++ Task_week2/DecisionTree.py
@@ -29,5 +29,32 @@ def divide(data, label, uqFeat):
     return new_data
 
 
+def optimize(data, label):
+    length = len(data)
+    feature_num = len(data[0])
+    originEnt = cal_ent(label)
+    feature = 0
+    maxGain = 0.0
+    special_value = 0
+    for i in range(feature_num):
+        value = [x[i] for x in data]
+        value = set(value)  # 一开始没想到用set...妙哇...
+        for mid_value in value:
+            new_data = divide(data, label, i)
+            len1 = len(label1)
+            len2 = len(label2)
+            if len1*len2 != 0:
+                ent1 = cal_ent(label1)
+                ent2 = cal_ent(label2)
+                gain = originEnt-(ent1*len1+ent2*len2)/length
+            else:
+                continue
+            #print('%d %f %f %f'%(i, mid_value,ent1,ent2))
+            if gain > maxGain:
+                maxGain = gain
+                feature = i
+                special_value = mid_value
+    return feature, special_value
+
 train_data, train_label = load('traindata.txt')
-ent = cal_ent(train_label)
+
