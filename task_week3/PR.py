0ba11620851f66ad927bda3d34a8c049d5a48934
creeper121386 <694029828@qq.com>
2018-05-06 21:42:42 +0800

    写BP算法ing...

--- task_week3/PR.py
+++ task_week3/PR.py
@@ -2,7 +2,7 @@ import matplotlib as plt
 import pandas as pd
 import numpy as np
 import math
-batchSize = 8
+batch = 8
 alpha = 0.01
 epoch = 100
 
@@ -17,22 +17,42 @@ class Net(object):
 
     def pred(self, input):
         tmp = input
+        nerve = [input]
         for i in range(self.num-1):
             tmp = sig(np.dot(tmp, self.weights[i])+self.bias[i])
-        return tmp
+            nerve.append(tmp)
+        return nerve
 
-    def BP(self, trainData, trainLabel, epoch, batchSize, alpha):
+    def update(self, label, nerve, alpha):
+        sigma = (label-nerve[-1])
+        for i in range(self.num-1):
+            grad = nerve[-1-i]*(-nerve[-1-i]+1)*sigma
+            gradMat = np.row_stack((grad,)*len(nerve[-2-i]))
+            Bn = np.column_stack((nerve[-2-i],)*len(nerve[-1-i]))
+            self.weights[-1-i] += alpha*gradMat*Bn
+            self.bias[-1-i] += -alpha*grad
+            sigma = np.sum(grad*self.weights[-1-i])
+
+    def BP(self, trainData, trainLabel, epoch, batch, alpha):
         for j in range(epoch):
-            for i in range(int(len(trainLabel)/batchSize)):
-                batchData = trainData[i:i+batchSize, :]
-                batchLabel = trainLabel[i:i+batchSize]
-                predLabel = [self.pred(x) for x in batchData]
-                loss = np.sum((batchLabel-np.array(predLabel))**2)/2
+            if batch:
+                for i in range(int(len(trainLabel)/batch)):
+                    batchData = trainData[i:i+batch, :]
+                    batchLabel = trainLabel[i:i+batch]
+                    nerve = [self.pred(x) for x in batchData]
+                    predLabel = [x[-1] for x in nerve]
+                    loss = np.sum((batchLabel-np.array(predLabel))**2)/2
+                    for x, y in zip(batchLabel, nerve):
+                        self.update(x, y, alpha)
+                    print('training: epoch', epoch, 'loss:', loss)
+                else:
+                    nerve = [self.pred(x) for x in trainData]
+                    for x, y in zip(trainLabel, nerve):
+                        self.update(x, y, alpha)
+                    print('train finished,loss:', loss)
 
-
-
-                self.weights+=deltaW
-                self.bias+=deltaB
+    def test():
+        pass
 
 def load(sheetNo):
     df = pd.read_excel(
@@ -48,7 +68,7 @@ def sig(x):
     return 1.0/(1.0+np.exp(-x))
 
 
+testData, teatLabel, cateNum = load(2)
 trainData, trainLabel, cateNum = load(1)
 nn = Net((5, 10, cateNum))
-nn.pred(trainData[1])
-nn.BP(trainData, trainLabel, epoch, batchSize, alpha)
+nn.BP(trainData, trainLabel, epoch, batch, alpha)
