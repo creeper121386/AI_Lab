61e8d059cd511b9dcc54b9d56b554ab43b46bb0e
creeper121386 <694029828@qq.com>
2018-06-10 20:49:54 +0800

    学习pytorch

--- task_week6/DCGAN.py
+++ task_week6/DCGAN.py
@@ -2,15 +2,18 @@ import torchvision.datasets as dSet
 import torchvision.transforms as T
 import torch.optim as optim
 import torch.nn as nn
+import torchvision
 import torch
 cuda = True
+epoch = 30
 lr = 0.01
 nz = 100        # 白噪声向量的长度
-nc = 3         # channel数
+nc = 1         # channel数
 ndf = 64      # 网络D的feature map数量
 ngf = 64       # 网络G的feature map数量
 imgSize = 28
 batchSize = 4
+device = torch.device("cuda:0" if cuda else "cpu")
 transform = T.Compose(
     [T.ToTensor(), T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 trainData = dSet.MNIST(root='/run/media/why/DATA/why的程序测试/AI_Lab/DataSet',
@@ -18,6 +21,8 @@ trainData = dSet.MNIST(root='/run/media/why/DATA/why的程序测试/AI_Lab/DataS
 trainLoader = torch.utils.data.DataLoader(dataset=trainData,
                                           batch_size=batchSize,
                                           shuffle=True)
+
+path = '/run/media/why/DATA/why的程序测试/AI_Lab/Task/task_week6/img'
 
 
 class D(nn.Module):
@@ -74,13 +79,57 @@ class G(nn.Module):
     def forward(self, input):
         return self.layers(input)
 
-
-
-
 gNet = G()
 dNet = D()
+if cuda:
+    dNet = dNet.cuda()
+    gNet = gNet.cuda()
 D_optim = optim.Adam(dNet.parameters(), lr=lr, betas=(0.5, 0.999))
 G_optim = optim.Adam(gNet.parameters(), lr=lr, betas=(0.5, 0.999))
+fixed_noise = torch.randn(batchSize, nz, 1, 1).cuda()
 criterion = nn.BCELoss()
+
+
+for j in range(epoch):
+    for i, data in enumerate(trainLoader, 0):
+        dNet.zero_grad()
+        real = data[0].to(device)
+        label = torch.full((batchSize,), 1).cuda()
+        output = dNet(real)
+        err_D = criterion(output, label)
+        err_D.backward()
+        Dx = output.mean().item()
+
+        noise = torch.randn(batchSize, nz, 1, 1).cuda()
+        fake = gNet(noise)
+        label.fill_(0)
+        output = dNet(fake.detach())
+        err_Dfake = criterion(output, label)
+        err_Dfake.backward()
+        z1 = output.mean().item()
+        errD = err_Dfake + err_D
+        D_optim.step()
+
+        gNet.zero_grad()
+        label.fill_(1)
+        output = dNet(fake)
+        errG = criterion(output, label)
+        errG.backward()
+        z2 = output.mean().item()
+        G_optim.step()
+
+        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+              % (j, epoch, i, len(trainLoader),
+                 errD.item(), errG.item(), Dx, z1, z2))
+
+        if i % 100 == 0:
+            torchvision.utils.save_image(real,
+                    '%s/real_samples.png' % path,
+                    normalize=True)
+            fake = gNet(fixed_noise)
+            torchvision.utils.save_image(fake.detach(),
+                    '%s/fake_samples_epoch_%03d.png' % (path, j),
+                    normalize=True)
+
 
 
---
+++ task_week6/whyの学习笔记 Pytorch.md
@@ -0,0 +1,35 @@
+# whyの学习笔记 Pytorch
+
+## 基本类
+
+###　张量（Tensor）:
+
+一个tensor实例有以下常用成员：
+
+* `Tensor.data`: 张量的数据本体
+* `Tensor.grad`: 该张量处的梯度（如果有的话）
+* `Tensor.grad_fn`: 指向在该张量处进行的函数计算（计算图中的计算节点），进行梯度反向传播时会用到。如果是由用户创建的tensor，则`grad_fn = None`。
+* `Tensor.grad_fn.next_function`: 上一级节点处的`grad_fn`。
+
+## Autograd
+
+* 设置`Tensor.requires_grad = True`时，pytorch会自动追踪对该张量进行的计算。只要调用`Tensor.backward()`即可反向计算出所有节点处的梯度。
+* 如果不想追踪对张量的计算， 可以使用：
+    * `Tensor.detach()`，把该张量从计算图中分离。
+    * 使用`with torch.no_grad():`来包装代码。使用示例：
+        ```python
+        with torch.no_grad():
+            [我是代码]
+        ```
+
+使用反向传播`Tensor.backward()`：
+
+* `Tensor.backward(gradient=grad)`： grad是一个张量，用来表示待计算的Tensor的各个元素的计算比例。(???存疑)
+* 注意：使用`.backward()`方法的张量必须是标量（只有一个元素）
+
+
+## `nn.Module`类
+
+* nn.Conv2d()支持的张量格式为：$batchSize \times channels \times height \times width$
+* 损失函数使用`nn.函数名`调用
+* 优化方法（adam什么的）在`nn.optim`中
