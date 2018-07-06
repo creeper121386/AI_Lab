d41fdf43f5c5b3f2b0f78edca6516b241dccddf4
creeper121386 <694029828@qq.com>
2018-07-06 11:32:37 +0800

    写infoGAN！

--- task_week6/DCGAN.py
+++ task_week6/DCGAN.py
@@ -9,16 +9,15 @@ cuda = True
 epoch = 10
 lr = 0.01       # 学习率
 nz = 100        # 白噪声向量的长度
-imgSize = 32    
+imgSize = 32
 batchSize = 16
 criterion = nn.BCELoss()
 device = torch.device("cuda" if cuda else "cpu")
 path = '/run/media/why/DATA/why的程序测试/AI_Lab/Task/task_week6/img'
 trainData = dSet.MNIST(root='/run/media/why/DATA/why的程序测试/AI_Lab/DataSet',
                        train=True, transform=T.Compose([T.Resize(imgSize), T.ToTensor()]), download=True)      # size = 28*28
-trainLoader = torch.utils.data.DataLoader(dataset=trainData,
-                                          batch_size=batchSize,
-                                          shuffle=True)
+trainLoader = torch.utils.data.DataLoader(
+    dataset=trainData, batch_size=batchSize, shuffle=True)
 
 
 gNet = G()
@@ -28,7 +27,8 @@ if cuda:
     gNet = gNet.cuda()
 D_optim = optim.Adam(dNet.parameters(), lr=lr, betas=(0.5, 0.999))
 G_optim = optim.Adam(gNet.parameters(), lr=lr, betas=(0.5, 0.999))
-fixed_noise = torch.randn(batchSize, nz, 1, 1).cuda()               # 用来生成假图片的噪声
+fixed_noise = torch.randn(
+    batchSize, nz, 1, 1).cuda()               # 用来生成假图片的噪声
 
 for j in range(epoch):
     for i, (img, _) in enumerate(trainLoader, 0):
@@ -39,7 +39,7 @@ for j in range(epoch):
         output = dNet(real)
         err_D = criterion(output, label)        # 计算loss
         err_D.backward()                        # 反向计算所有梯度
-        #　Dx = output.mean().item()               
+        #　Dx = output.mean().item()
 
         noise = torch.randn(batchSize, nz, 1, 1, device=device)     # 创建噪声向量
         fake = gNet(noise)                      # 开始造假！
@@ -47,23 +47,25 @@ for j in range(epoch):
         output = dNet(fake.detach())            # 识别假图片（不计算G中参数的梯度）
         err_Dfake = criterion(output, label)    # 计算loss
         err_Dfake.backward()                    # 反向计算梯度
-        # z1 = output.mean().item()              
+        # z1 = output.mean().item()
         errD = err_Dfake + err_D                # 综合考虑loss
         D_optim.step()                          # 更新识别器的参数
         # 训练生成器：
         gNet.zero_grad()                        # 初始化梯度
-        label.fill_(1)                          # 为所有label标记1（要使得造假后的图片输入D后，输出的结果更接近1）
+        # 为所有label标记1（要使得造假后的图片输入D后，输出的结果更接近1）
+        label.fill_(1)
         output = dNet(fake)                     # 识别假图片
         errG = criterion(output, label)         # 计算loss
         errG.backward()                         # 反向计算梯度
-        # z2 = output.mean().item()               
+        # z2 = output.mean().item()
         G_optim.step()                          # 更新生成器的参数
         # 输出、保存训练信息
         if not(i % 100):
             print('epoch:[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f'
                   % (j, epoch, i, len(trainLoader), errD.item(), errG.item()))
-            torchvision.utils.save_image(real,'%s/realSamples.png' % path,normalize=True)
+            torchvision.utils.save_image(
+                real, '%s/realSamples.png' % path, normalize=True)
             fake = gNet(fixed_noise)
             torchvision.utils.save_image(fake.detach(),
-                '%s/fakeSamples_epoch_%03d.png' % (path, j),normalize=True)
+                                         '%s/fakeSamples_epoch_%03d.png' % (path, j), normalize=True)
 print('traning finished!')
