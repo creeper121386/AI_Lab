6308728167effbf13fbe9d6affdc4edfb884bab7
creeper121386 <694029828@qq.com>
2018-07-20 17:14:20 +0800

    继续训练ing

---
+++ Task_Extra/avator/connect.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+
+#scp -r remote unique@115.156.207.244:/disk/unique/why/data .
+#scp -r remote unique@115.156.207.244:/disk/unique/why/model .
--- Task_Extra/avator/test.py
+++ Task_Extra/avator/test.py
@@ -3,36 +3,53 @@ import torch
 import torchvision
 device = torch.device(
     'cuda') if torch.cuda.is_available() else torch.device('cpu')
+randZ = True
 nz = 100
 batchSize = 64
-batchNum = 1
+batchNum = 2
 # modelNum = [x*10+1 for x in range(10)]+[100]
-modelNum = [91, ]
-randZ = True
-tmp = torch.randn(nz, 1, 1)
-initZ = torch.randn(batchSize, nz, 1, 1)
-savePath = '/run/media/why/DATA/why的程序测试/AI_Lab/Task/Task_Extra/avator/img'
+modelNum = [6, 11, 16, 20]
+workDir = '/run/media/why/DATA/why的程序测试/AI_Lab/Task/Task_Extra/avator'
+savePath = workDir + '/img'
+modelPath = workDir + '/model-epoch100'
+#################################################
 
+class TestNet(object):
+    def __init__(self):
+        self.G = G().to(device)
+        self.batchNum = batchNum
+################ init noise code z: #################
 
-for i in range(nz-1):
-    for j in range(batchSize):
-        initZ[j][i] = tmp[i]
-initZ = initZ.to(device)
+    def init(self):
+        if not randZ:
+            self.batchNum = 1
+            tmp = torch.randn(nz, 1, 1)
+            initZ = torch.randn(batchSize, nz, 1, 1)
+            for i in range(nz-5):
+                for j in range(batchSize):
+                    initZ[j][i] = tmp[i]
+            self.initZ = initZ.to(device)
 
+###################### test:#########################
+    def test(self):
+        for n in modelNum:
+            GmodelPath = modelPath + '/Gnn-epoch{}.pkl'.format(n)
+            self.G.load_state_dict(torch.load(GmodelPath))
+            for i in range(batchNum):
+                if randZ:
+                    z = torch.randn(batchSize, nz, 1, 1).to(device)
+                else:
+                    z = self.initZ
+                torchvision.utils.save_image(self.G(z).detach(
+                ), savePath+'/epoch{}-test{}.jpg'.format(n, i), normalize=True)
+        print('\033[1;36;40m  generate over! \033[0m')
 
+############## load model: ###############
 #Dnn = D()
 # Dnn.load_state_dict(torch.load(modelPath))
-Gnn = G().to(device)
 
-for n in modelNum:
-    GmodelPath = '/run/media/why/DATA/why的程序测试/AI_Lab/Task/Task_Extra/avator/model/Gnn-epoch{}.pkl'.format(
-        n)
-    Gnn.load_state_dict(torch.load(GmodelPath))
-    for i in range(batchNum):
-        if randZ:
-            z = torch.randn(batchSize, nz, 1, 1).to(device)
-        else:
-            z = initZ
-        out = Gnn(z)
-        torchvision.utils.save_image(Gnn(z).detach(
-        ), savePath+'/epoch{}-test{}.jpg'.format(n, i), normalize=True)
+
+if __name__ == '__main__':
+    testNet = TestNet()
+    testNet.init()
+    testNet.test()
